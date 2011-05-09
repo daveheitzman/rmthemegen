38eff20aa6410a975618d20d440d0778e5214cc6
David Heitzman <daveheitzman@yahoo.com>
2011-05-09 14:59:56 -0500

    fixed #!/user/bin/env ruby

--- bin/generate_themes.rb
+++ bin/generate_themes.rb
@@ -1,4 +1,4 @@
-#! /usr/bin/env/ruby 
+#! /usr/bin/env ruby 
 #**********************************************************************
 #*                                                                    *
 #*  RmThemeGen - a ruby script to create random, usable themes for    *
