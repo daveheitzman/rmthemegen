3fa3be84556f50b710c33db3642075ea294a31bd
David Heitzman <daveheitzman@yahoo.com>
2011-05-18 12:44:26 -0500

    compiles list of entity tags used across all the .tmTheme files

--- test/get_tm_tokens.rb
+++ test/get_tm_tokens.rb
@@ -32,19 +32,35 @@ Kernel.exit
 tm_files = Dir.glob("*.tmTheme")
 puts tm_files.inspect
 @doc_opts_accum = Hash.new
+@element_tokens = {}
+@element_scopes = {}
+
 tm_files.each do |inf|
   puts "xml_in for "+inf.to_s
   @inf = File.open(inf,"r")
   @tm_xml_in = XmlSimple.xml_in(@inf)
-#  puts @tm_xml_in.inspect
-#  puts @tm_xml_in["dict"].inspect  
+  puts @tm_xml_in.inspect
+  
   @doc_opts_accum.merge!( @tm_xml_in["dict"][0]["array"][0]["dict"][0]["dict"][0])
-#  puts @tm_xml_in.inspect
-  @outf = File.new("zxzxzxzxzx.tmTheme","w+")
-  XmlSimple.xml_out(@tm_xml_in, {:keeproot=>true,:xmldeclaration=>true,:outputfile=> @outf, :rootname => "scheme"})
-  @outf.close
+  
+  @tm_xml_in["dict"][0]["array"][0]["dict"].each_index do |i|  
+    #find index of "scope" in array "key"
+#    puts i.to_s+" |e| |e| "+@tm_xml_in["dict"][0]["array"][0]["dict"][i]["string"][1].inspect if @tm_xml_in["dict"][0]["array"][0]["dict"][i]["string"]
+    @element_tokens[@tm_xml_in["dict"][0]["array"][0]["dict"][i]["string"][1].to_s]=1 if @tm_xml_in["dict"][0]["array"][0]["dict"][i]["string"]
+#    @element_tokens.add( @tm_xml_in["dict"][0]["array"][0]["dict"][i]["string"][1].to_s]) if @tm_xml_in["dict"][0]["array"][0]["dict"][i]["string"]
+
+    #scope_ind = e[:key].index_of["scope"]
+    #puts "scope_ind = "+scope_ind
+       
+  end 
+
+#  @outf = File.new("zxzxzxzxzx.tmTheme","w+")
+#  XmlSimple.xml_out(@tm_xml_in, {:keeproot=>true,:xmldeclaration=>true,:outputfile=> @outf, :rootname => "scheme"})
+#  @outf.close
 end
   puts "@doc_opts_accum: "+@doc_opts_accum.inspect
+  puts "@element_tokens: "+@element_tokens.inspect
+  puts "@element_scopes: "+@element_scopes.inspect
 
 
 Kernel.exit
